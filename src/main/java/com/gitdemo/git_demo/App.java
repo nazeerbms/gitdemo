d5095ec4992a20db1592d60a7f1f4b094acd53e7
nazeerbms <nazeerbms@gmail.com>
2024-01-02 03:24:10 +0530

    added adddata method in code

--- src/main/java/com/gitdemo/git_demo/App.java
+++ src/main/java/com/gitdemo/git_demo/App.java
@@ -6,6 +6,15 @@ package com.gitdemo.git_demo;
  */
 public class App 
 {
+	public void addData(String data) {
+		
+		System.out.println("Print data" + data);
+		if (data.equals("git demo")) {
+			System.out.println("Welcome to git demo");
+		}
+		
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
